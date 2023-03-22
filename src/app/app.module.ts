94051407b0f3ef4c0829e63737e22fbb46029eb7
Jessi Sandoval <jessicaandreasandoval@gmail.com>
2023-03-22 00:27:40 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuIzComponent } from './menu-iz/menu-iz.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AreaDeContenidoComponent } from './area-de-contenido/area-de-contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuIzComponent,
+    ToolbarComponent,
+    AreaDeContenidoComponent
   ],
   imports: [
     BrowserModule
