8acfdbfcd995526af80a501dc1e3693376b4f525
Jessi Sandoval <jessicaandreasandoval@gmail.com>
2023-03-29 19:11:53 -0300

    primer commit desafio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuIzComponent } from './menu-iz/menu-iz.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { AreaDeContenidoComponent } from './area-de-contenido/area-de-contenido.component';
+import { DiretivasComponent } from './diretivas/diretivas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuIzComponent,
     ToolbarComponent,
-    AreaDeContenidoComponent
+    AreaDeContenidoComponent,
+    DiretivasComponent
   ],
   imports: [
     BrowserModule
